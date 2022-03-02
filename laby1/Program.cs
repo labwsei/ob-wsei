18a5182774790c172e8adcdf7ce97421e7f24270
Jan Ślażyński <jan.slazynski@microsoft.wsei.edu.pl>
2022-03-02 10:29:18 +0100

    Initial

---
+++ laby1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laby1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
