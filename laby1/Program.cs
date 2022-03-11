96c177af4aadf3489b501a7c4602088ac1395979
net-runner <slazynski.j@gmail.com>
2022-03-11 17:06:07 +0100

    Cw 11

--- laby1/Program.cs
+++ laby1/Program.cs
@@ -2,46 +2,51 @@
 
 namespace laby1
 {
-  partial class Program
-  {
-    static void Main(string[] args)
+    partial class Program
     {
-      Money money = Money.Of(10, Currency.PLN);
-      Money result = money * 5.2m;
-      Console.WriteLine(result.Value);
-      Money sum = money + result;
-      Money sum2 = sum;
-      Console.WriteLine(sum2.Value);
+        static void Main(string[] args)
+        {
+            Money money = Money.Of(10, Currency.PLN);
+            Money result = money * 5.2m;
+            Console.WriteLine(result.Value);
+            Money sum = money + result;
+            Money sum2 = sum;
+            Console.WriteLine(sum2.Value);
 
-      Tank tank1 = new Tank(30);
-      Tank tank2 = new Tank(50);
-      tank1.refuel(30);
-      tank2.refuel(25);
-      tank2.refuel(tank1, 10);
-      Console.WriteLine(tank2);
-      Console.WriteLine(tank1);
+            Tank tank1 = new Tank(30);
+            Tank tank2 = new Tank(50);
+            tank1.refuel(30);
+            tank2.refuel(25);
+            tank2.refuel(tank1, 10);
+            Console.WriteLine(tank2);
+            Console.WriteLine(tank1);
 
-      Console.WriteLine(money.ToString());
+            Console.WriteLine(money.ToString());
 
+            Money pircent = Money.Of(100, Currency.PLN);
+            Console.WriteLine(pircent.Percent(23.45m));
 
-      Money[] bank =
-{
+            //Cw11
+            var resoultador = pircent.ToCurrency(Currency.PLN, 4.1m);
+
+            Money[] bank =
+      {
     Money.Of(3, Currency.PLN),
     Money.Of(3m, Currency.USD),
     Money.Of(1m, Currency.PLN),
     Money.Of(4m, Currency.USD),
 };
 
-      Array.Sort(bank);
-      for (int i = 0; i < bank.Length; i++)
+            Array.Sort(bank);
+            for (int i = 0; i < bank.Length; i++)
+            {
+                Console.WriteLine(bank[i].ToString());
+            }
+
+
+
+            Student[] students =
       {
-        Console.WriteLine(bank[i].ToString());
-      }
-
-
-
-      Student[] students =
-{
     new Student("Bingus", "Dingus", 6),
     new Student("Amogus", "Zingus", 10),
     new Student("Śmingus", "Roingus", 2.5m),
@@ -49,11 +54,11 @@ namespace laby1
     new Student("Gingus", "Kongus", 12),
 };
 
-      Array.Sort(students);
-      for (int i = 0; i < students.Length; i++)
-      {
-        Console.WriteLine(students[i]);
-      }
+            Array.Sort(students);
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+        }
     }
-  }
 }
